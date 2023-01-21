5501189eec00b4444318f1f7fd618ba2fa978cfc
Pritish Budhiraja - Juspay <pritish.budhiraja@juspay.in>
2023-01-21 18:26:00 +0530

    Favicon Addded.

---
+++ build/precache-manifest.0caa9a280755997e71f68122942bb2e0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a83ca9e94d8da024043bc05dbb37be2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "288ee4865b15a89b1463",
+    "url": "/static/css/main.ab604968.chunk.css"
+  },
+  {
+    "revision": "832f07514361a4d12ea4",
+    "url": "/static/js/2.955320e6.chunk.js"
+  },
+  {
+    "revision": "cbbbfaeef69679fad646c82c8b62a4d5",
+    "url": "/static/js/2.955320e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "288ee4865b15a89b1463",
+    "url": "/static/js/main.e61df085.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.e61df085.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6022a992b1f9af4d30eb",
+    "url": "/static/js/runtime-main.3f2fd132.js"
+  }
+]);
